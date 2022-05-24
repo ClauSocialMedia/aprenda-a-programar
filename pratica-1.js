0ec8d5a6ea67a76d197cee567917ac5568281900
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-05-24 20:22:25 -0300

    apanhando do git

---
+++ pratica-1.js
@@ -0,0 +1,13 @@
+let nome
+let profissao
+let email
+let idade
+let lugar
+
+nome = "claudia"
+profissao = "supervisora"
+email = "claudaipiurkoski@gmail.com"
+idade = 46
+lugar = "noronha"
+
+console.log({nome,profissao,email,idade,lugar})
